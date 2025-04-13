a28b860a1e5ab2ff3d08433f6c8ddfc62ad2b32e
joshuajpiluden@gmail.com <joshuajpiluden@Gmail.com>
2025-04-13 16:18:22 +0800

    Test OllamaChatClient with MCP

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,30 +1,8 @@
-﻿using ModelContextProtocol.Client;
+﻿
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Client;
 using ModelContextProtocol.Protocol.Transport;
-
-
-// await using var mcpSseClient = await McpClientFactory.CreateAsync(new()
-// {
-//     Id = "TestServerSse",
-//     Name = "TestServer",
-//     TransportType = TransportTypes.Sse,
-//     Location = "http://localhost:5021/sse",
-// });
-
-// var tools = await mcpSseClient.ListToolsAsync();
-// foreach (var tool in tools)
-// {
-//     Console.WriteLine($"Connected to server with tools: {tool.Name}");
-// }
-
-// var result = await mcpSseClient.CallToolAsync(
-//            "Echo",
-//            new Dictionary<string, object?>
-//            {
-//                ["message"] = "Hello MCP!"
-//            },
-//            default);
-
-// Console.WriteLine($"Result: {result.Content.First().Text}");
 
 
 
@@ -45,16 +23,47 @@ foreach (var tool in tools)
     Console.WriteLine($"Connected to server with tools: {tool.Name}");
 }
 
-var result = await mcpClient.CallToolAsync(
-           "GetAnimeListBySeasonAndYear",
-           new Dictionary<string, object?>
-           {
-               ["season"] = "winter",
-               ["year"] = 2024
-           },
-           default);
+// var result = await mcpClient.CallToolAsync(
+//            "GetAnimeListBySeasonAndYear",
+//            new Dictionary<string, object?>
+//            {
+//                ["season"] = "winter",
+//                ["year"] = 2024
+//            },
+//            default);
 
-Console.WriteLine($"Result: {result.Content.First().Text}");
+// Console.WriteLine($"Result: {result.Content.First().Text}");
+
+
+// This model is not enough to give any coherent response
+var ollamaChatClient = new OllamaChatClient(new Uri("http://localhost:11434"), "llama3.2:3b");
+
+using var factory =
+    LoggerFactory.Create(builder => builder.AddConsole().SetMinimumLevel(LogLevel.None));
+
+var client = new ChatClientBuilder(ollamaChatClient)
+    .UseLogging(factory)
+    .UseFunctionInvocation()
+    .Build();
+
+
+IList<ChatMessage> messages =
+[
+   new(ChatRole.System, """
+                        You're an otaku who knows everything there is to know about anime.
+                        """),
+    new(ChatRole.User, "What are the best anime of the winter 2024 season?"),
+];
+
+var response =
+    await client.GetResponseAsync(
+        messages,
+        new ChatOptions
+        {
+            Tools = [.. tools]
+        });
+
+Console.WriteLine(response);
 
 static (string command, string[] arguments) GetCommandAndArguments(string[] args)
 {
