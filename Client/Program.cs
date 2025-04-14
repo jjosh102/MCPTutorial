b3f945253d9ae19f91b5d3988642fde3bd8b3658
joshuajpiluden@gmail.com <joshuajpiluden@Gmail.com>
2025-04-15 07:54:48 +0800

    Integrate Semantic Kernel using agents

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,7 @@
 ﻿
-using Microsoft.Extensions.AI;
-using Microsoft.Extensions.Logging;
+
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Agents;
 using ModelContextProtocol.Client;
 using ModelContextProtocol.Protocol.Transport;
 
@@ -36,34 +37,65 @@ foreach (var tool in tools)
 
 
 // This model is not enough to give any coherent response
-var ollamaChatClient = new OllamaChatClient(new Uri("http://localhost:11434"), "llama3.2:3b");
+// var ollamaChatClient = new OllamaChatClient(new Uri("http://localhost:11434"), "llama3.2:3b");
 
-using var factory =
-    LoggerFactory.Create(builder => builder.AddConsole().SetMinimumLevel(LogLevel.None));
+// using var factory =
+//     LoggerFactory.Create(builder => builder.AddConsole().SetMinimumLevel(LogLevel.None));
 
-var client = new ChatClientBuilder(ollamaChatClient)
-    .UseLogging(factory)
-    .UseFunctionInvocation()
-    .Build();
+// var client = new ChatClientBuilder(ollamaChatClient)
+//     .UseLogging(factory)
+//     .UseFunctionInvocation()
+//     .Build();
 
 
-IList<ChatMessage> messages =
-[
-   new(ChatRole.System, """
-                        You're an otaku who knows everything there is to know about anime.
-                        """),
-    new(ChatRole.User, "What are the best anime of the winter 2024 season?"),
-];
+// IList<ChatMessage> messages =
+// [
+//    new(ChatRole.System, """
+//                         You're an otaku who knows everything there is to know about anime.
+//                         """),
+//     new(ChatRole.User, "What are the best anime of the winter 2024 season?"),
+// ];
 
-var response =
-    await client.GetResponseAsync(
-        messages,
-        new ChatOptions
-        {
-            Tools = [.. tools]
-        });
+// var response =
+//     await client.GetResponseAsync(
+//         messages,
+//         new ChatOptions
+//         {
+//             Tools = [.. tools]
+//         });
 
-Console.WriteLine(response);
+// Console.WriteLine(response);
+
+var builder = Kernel.CreateBuilder();
+
+builder.AddOllamaChatCompletion("llama3.2:3b", new Uri("http://localhost:11434"));
+
+var kernel = builder.Build();
+
+// Using the agent can understand better compared to the chat client,
+// but it is still unusable. It is better to stick to Copilot's free agent support.
+ChatCompletionAgent agent = new()
+{
+    Instructions =
+    """
+            You're an otaku who knows everything there is to know about anime.
+            """,
+    Name = "Anime Agent",
+    Kernel = kernel,
+
+    Arguments = new KernelArguments(new PromptExecutionSettings
+    { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() })
+};
+
+kernel.Plugins.AddFromFunctions("Demo", tools.Select(tools => tools.AsKernelFunction()));
+
+ChatHistoryAgentThread agentThread = new();
+
+await foreach (StreamingChatMessageContent response in agent.InvokeStreamingAsync("What are the best anime of the winter 2024 season?", agentThread))
+{
+    Console.WriteLine(response.Content);
+}
+
 
 static (string command, string[] arguments) GetCommandAndArguments(string[] args)
 {
